3b9df5dc173a837fa37e8c9b6c70c4736c1d12e2
Intersected2 <brianx8@nycstudents.net>
2025-10-22 15:34:56 -0400

    yes

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,7 +1,7 @@
 public class APCalendar
 {
     /** Returns true if year is a leap year and false otherwise. */
-    private boolean isLeapYear(int year){
+    private static boolean isLeapYear(int year){
         if (year % 4 == 0) {
             return true;
         }
@@ -12,26 +12,44 @@ public class APCalendar
     /** Returns the number of leap years between year1 and year2, inclusive.
      * Precondition: 0 <= year1 <= year2
      */
-    public int numberOfLeapYears(int year1, int year2){
+    public static int numberOfLeapYears(int year1, int year2){
         int x = year2 - year1;
         int ly = 0;
-        while (x >= 4){
-            x = x - 4;
-            ly = ly + 1;
+        if (year2 > year1) {
+            while (x >= 4) {
+                x = x - 4;
+                ly = ly + 1;
+            }
+        }else {
+            x = year1 - year2;
+            while (x >= 4) {
+                x = x - 4;
+                ly = ly + 1;
+            }
         }
         return ly;
     }
     /** Returns the value representing the day of the week for the first day of year,
      * where 0 denotes Sunday, 1 denotes Monday, ..., and 6 denotes Saturday.
      */
-    private int firstDayOfYear(int year){
-    return 0;
+    private static int firstDayOfYear(int year){
+        int start = 2019;
+        int startw= 2;
+        int compare;
+        if (year > 2019){
+             compare = year - 2019;
+
+        }
+        else if (year < 2019){
+            compare = year - 2019;
+        }
+        return 0;
     }
     /** Returns n, where month, day, and year specify the nth day of the year.
      * Returns 1 for January 1 (month = 1, day = 1) of any year.
      * Precondition: The date represented by month, day, year is a valid date.
      */
-    private int dayOfYear(int month, int day, int year){
+    private static int dayOfYear(int month, int day, int year){
     return 0;
     }
     /** Returns the value representing the day of the week for the given date
@@ -39,7 +57,7 @@ public class APCalendar
      * and 6 denotes Saturday.
      * Precondition: The date represented by month, day, year is a valid date.
      */
-    public int dayOfWeek(int month, int day, int year){
+    public static int dayOfWeek(int month, int day, int year){
     return 0;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,5 @@
 public class main {
     public static void main(String []args){
-        APCalendar cal = new APCalendar();
-        System.out.println(cal.numberOfLeapYears(2020,2024));
+        System.out.println(APCalendar.numberOfLeapYears(2024,2020));
     }
 }
