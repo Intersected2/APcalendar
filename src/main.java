d448454fea62187e5e0d5341ce152223134b3e62
Intersected2 <brianx8@nycstudents.net>
2025-10-22 15:18:43 -0400

    yes

--- src/main.java
+++ src/main.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class main {
+    public static void main(String []args){
+        APCalendar cal = new APCalendar();
+        System.out.println(cal.numberOfLeapYears(2020,2024));
+    }
 }
